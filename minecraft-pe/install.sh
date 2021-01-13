5c040976145838f6731e33a47e4fb2cf07e3010d
Breno Ribeiro do Valle <28983571+brenordv@users.noreply.github.com>
2021-01-12 21:56:46 -0300

    trying something different
    kind gave up on MineOS @ CaC... 
    Minecraft PE is working pretty well...

---
+++ minecraft-pe/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt-get update -y
+apt-get upgrade -y
+ufw allow 19132/tcp
+apt-get install build-essential libtool-bin autoconf bison screen -y
+useradd -d /home/minecraft -m minecraft
+passwd minecraft
+su minecraft
+cd ~
+wget -q -O - https://get.pmmp.io | bash -s -
+script /dev/null
+screen ./start.sh
+su root
+line="@reboot /home/minecraft/start.sh"
+(crontab -u root -l; echo "$line" ) | crontab -u root -
